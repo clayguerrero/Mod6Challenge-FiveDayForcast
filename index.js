a12c873728a73551239e6019d74d250a179100ab
clayg <claytondguerrero@gmail.com>
2023-09-12 16:49:17 -0500

    created the addToCityList function, still need to complete functionality

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const searchBtn = $(".search");
+const history = $('.history')
+const historyItem = $('.historyItem')
+// const citySearch = $(".citySearcher");
 
-searchBtn.css("background-color", "red");
+// searchBtn.css("background-color", "red");
+
+$(function addToCityList() {
+  searchBtn.on("click", function (e) {
+      e.preventDefault();
+    //   const newHistItem = '<li><button class="historyItem"><h4></h4></button></li>'
+      const newCityName = $(this).siblings("input").val()
+      history.append(li)
+
+    //   .append('h4').text($(this).siblings("input").val()).addClass('historyItem');
+
+    // console.log($(this));
+    // console.log("hello");
+  });
+});

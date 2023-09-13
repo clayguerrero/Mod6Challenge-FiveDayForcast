6a40fa911196992f6fa6aa6eab7a828d64a834b0
clayg <claytondguerrero@gmail.com>
2023-09-13 12:42:09 -0500

    historyItems created on click

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const searchBtn = $(".search");
-const history = $('.history')
-const historyItem = $('.historyItem')
+const history = $(".history");
+const historyItem = $(".historyItem");
+let count = 0;
 // const citySearch = $(".citySearcher");
 
 // searchBtn.css("background-color", "red");
 
+const dummyCity = [];
+
 $(function addToCityList() {
   searchBtn.on("click", function (e) {
-      e.preventDefault();
-    //   const newHistItem = '<li><button class="historyItem"><h4></h4></button></li>'
-      const newCityName = $(this).siblings("input").val()
-      history.append(li)
-
-    //   .append('h4').text($(this).siblings("input").val()).addClass('historyItem');
-
-    // console.log($(this));
-    // console.log("hello");
+    e.preventDefault();
+    const newCityName = $(this).siblings("input").val();
+    dummyCity.push(newCityName);
+    history.append("<li><button class='historyItem'><h4></h4></button></li>");
+    dummyCity.forEach((city) => {
+      const currCity = $(this).parent().siblings("ul").find("h4");
+      currCity[count].textContent = dummyCity[count];
+      console.log(dummyCity[count]);
+    });
+    count += 1;
+    $(this).siblings("input").val("");
+    console.log(dummyCity);
   });
 });

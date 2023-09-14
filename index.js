901419befbff776de752398f18a0fa503abc3f33
clayg <claytondguerrero@gmail.com>
2023-09-14 17:04:30 -0500

    searching an city creates a new history button

--- index.js
+++ index.js
@@ -2,25 +2,34 @@ const searchBtn = $(".search");
 const history = $(".history");
 const historyItem = $(".historyItem");
 let count = 0;
+let cityName;
+let currCity;
+
 // const citySearch = $(".citySearcher");
 
 // searchBtn.css("background-color", "red");
 
-const dummyCity = [];
+let dummyCity = [];
 
 $(function addToCityList() {
+  const cityHistory = JSON.parse(localStorage.getItem("prevCity"));
+  if (cityHistory) {
+    dummyCity = cityHistory;
+  }
   searchBtn.on("click", function (e) {
     e.preventDefault();
     const newCityName = $(this).siblings("input").val();
     dummyCity.push(newCityName);
-    history.append("<li><button class='historyItem'><h4></h4></button></li>");
-    dummyCity.forEach((city) => {
-      const currCity = $(this).parent().siblings("ul").find("h4");
-      currCity[count].textContent = dummyCity[count];
-      console.log(dummyCity[count]);
-    });
-    count += 1;
-    $(this).siblings("input").val("");
-    console.log(dummyCity);
+    localStorage.setItem("prevCity", JSON.stringify(dummyCity));
+    location.reload()
   });
+  if (localStorage.prevCity) {
+    const prevCityList = JSON.parse(localStorage.prevCity);
+    prevCityList.forEach((city) => {
+      history.append("<li><button class='historyItem'><h4></h4></button></li>");
+      history.children().last().children().children().text(city);
+      count++;
+    });
+  }
+  // localStorage.clear()
 });

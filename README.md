ba9f76c1aaef3b4d2afaddb0972786f2e7fe3376
clayg <claytondguerrero@gmail.com>
2023-09-15 18:10:16 -0500

    can retreive weather data for houston

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 
 # External Sources
 * [Blue Gradiant](https://gradients.shecodes.io/gradients/801#gradient)
+* [5 Day Weather OpenWeather API](https://openweathermap.org/forecast5)
+* [JavaScript.PlainEnglish](https://javascript.plainenglish.io/display-7-day-weather-forecast-with-openweather-api-aac8ba21c9e3)
--- index.js
+++ index.js
@@ -3,16 +3,32 @@ const history = $(".history");
 const historyItem = $(".historyItem");
 let cityName;
 let currCity;
-let count
+let count;
 
+const apikey = 'c7edca2b5da386146c92e6e9f3694e5f';
+const dlat = '29.7633'
+const dlon = '-95.3633'
 // const citySearch = $(".citySearcher");
 
 // searchBtn.css("background-color", "red");
 
 let dummyCity = [];
-const getWeather = function () {
-  let apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + LAT + '&lon='+ LON + '&appid=' + APIKEY +  ''
-}
+let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${dlat}&lon=${dlon}&appid=${apikey}&units=imperial`;
+// const houUrl = `api.openweathermap.org/data/2.5/forecast?q=Houston&appid=${apikey}`
+
+// $.ajax({
+//   url: apiUrl,
+//   method: 'GET'
+// }).then(function (res) {
+//   console.log(res.list[0].main)
+// })
+
+fetch(apiUrl)
+  .then(function (res) {
+    return res.json()}
+).then(function (data) {
+      console.log(data)
+    })
 
 $(function addToCityList() {
   const cityHistory = JSON.parse(localStorage.getItem("prevCity"));
@@ -26,19 +42,19 @@ $(function addToCityList() {
     localStorage.setItem("prevCity", JSON.stringify(dummyCity));
     location.reload();
   });
-  lastTen()
+  lastTen();
   // localStorage.clear()
 });
 function lastTen() {
- if (localStorage.prevCity) {
+  if (localStorage.prevCity) {
     const prevCityList = JSON.parse(localStorage.prevCity);
     prevCityList.forEach((city) => {
       history.append("<li><button class='historyItem'><h4></h4></button></li>");
       history.children().last().children().children().text(city);
       if (history.children().length > 10) {
-        history.children().children().last().remove()
+        history.children().children().last().remove();
       }
     });
-    console.log(prevCityList)
+    console.log(prevCityList);
   }
 }
